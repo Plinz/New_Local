371a62732bda26bc66a178422cccf40bb060f3d6
Plinz <antoine.duquennoy@protonmail.com>
2018-11-28 10:21:14 +0100

    fix sql script broke test

--- src/main/webapp/app/account/register/register.service.ts
+++ src/main/webapp/app/account/register/register.service.ts
@@ -8,7 +8,10 @@ import { SERVER_API_URL } from 'app/app.constants';
 export class Register {
     constructor(private http: HttpClient) {}
 
-    save(account: any): Observable<any> {
+    save(account: any, seller: boolean): Observable<any> {
+        if (seller) {
+            account.authorities = 'ROLE_SELLER';
+        }
         return this.http.post(SERVER_API_URL + 'api/register', account);
     }
 }
