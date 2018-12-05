94cf1aebba108456e346c6b2aba5520befe345a1
Stephanie Barona <baronas@F202-13.e-im2ag.ujf-grenoble.fr>
2018-12-05 11:21:02 +0100

    holding

--- src/main/webapp/app/entities/holding/holding-update.component.ts
+++ src/main/webapp/app/entities/holding/holding-update.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { JhiAlertService } from 'ng-jhipster';
+import { JhiAlertService, JhiDataUtils } from 'ng-jhipster';
 
 import { IHolding } from 'app/shared/model/holding.model';
 import { HoldingService } from './holding.service';
-import { IImage } from 'app/shared/model/image.model';
-import { ImageService } from 'app/entities/image';
 import { ILocation } from 'app/shared/model/location.model';
 import { LocationService } from 'app/entities/location';
 import { IUser, UserService } from 'app/core';
@@ -20,19 +18,17 @@ export class HoldingUpdateComponent implements OnInit {
     holding: IHolding;
     isSaving: boolean;
 
-    images: IImage[];
-    holdingsCurrentUser: IHolding[];
-
     locations: ILocation[];
 
     users: IUser[];
 
     constructor(
+        private dataUtils: JhiDataUtils,
         private jhiAlertService: JhiAlertService,
         private holdingService: HoldingService,
-        private imageService: ImageService,
         private locationService: LocationService,
         private userService: UserService,
+        private elementRef: ElementRef,
         private activatedRoute: ActivatedRoute
     ) {}
 
@@ -41,27 +37,6 @@ export class HoldingUpdateComponent implements OnInit {
         this.activatedRoute.data.subscribe(({ holding }) => {
             this.holding = holding;
         });
-        this.holdingService.findByCurrentUser().subscribe(
-            (res: HttpResponse<IHolding[]>) => {
-                this.holdingsCurrentUser = res.body;
-            },
-            (res: HttpErrorResponse) => this.onError(res.message)
-        );
-        this.imageService.query({ 'holdingId.specified': 'false' }).subscribe(
-            (res: HttpResponse<IImage[]>) => {
-                if (!this.holding.image || !this.holding.image.id) {
-                    this.images = res.body;
-                } else {
-                    this.imageService.find(this.holding.image.id).subscribe(
-                        (subRes: HttpResponse<IImage>) => {
-                            this.images = [subRes.body].concat(res.body);
-                        },
-                        (subRes: HttpErrorResponse) => this.onError(subRes.message)
-                    );
-                }
-            },
-            (res: HttpErrorResponse) => this.onError(res.message)
-        );
         this.locationService.query().subscribe(
             (res: HttpResponse<ILocation[]>) => {
                 this.locations = res.body;
@@ -74,6 +49,22 @@ export class HoldingUpdateComponent implements OnInit {
             },
             (res: HttpErrorResponse) => this.onError(res.message)
         );
+    }
+
+    byteSize(field) {
+        return this.dataUtils.byteSize(field);
+    }
+
+    openFile(contentType, field) {
+        return this.dataUtils.openFile(contentType, field);
+    }
+
+    setFileData(event, entity, field, isImage) {
+        this.dataUtils.setFileData(event, entity, field, isImage);
+    }
+
+    clearInputImage(field: string, fieldContentType: string, idInput: string) {
+        this.dataUtils.clearInputImage(this.holding, this.elementRef, field, fieldContentType, idInput);
     }
 
     previousState() {
@@ -104,10 +95,6 @@ export class HoldingUpdateComponent implements OnInit {
 
     private onError(errorMessage: string) {
         this.jhiAlertService.error(errorMessage, null, null);
-    }
-
-    trackImageById(index: number, item: IImage) {
-        return item.id;
     }
 
     trackLocationById(index: number, item: ILocation) {
--- src/main/webapp/app/entities/holding/holding.component.ts
+++ src/main/webapp/app/entities/holding/holding.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
+import { JhiEventManager, JhiAlertService, JhiDataUtils } from 'ng-jhipster';
 
 import { IHolding } from 'app/shared/model/holding.model';
 import { Principal } from 'app/core';
-
-import { ITEMS_PER_PAGE } from 'app/shared';
 import { HoldingService } from './holding.service';
 
 @Component({
@@ -15,46 +13,19 @@ import { HoldingService } from './holding.service';
     templateUrl: './holding.component.html'
 })
 export class HoldingComponent implements OnInit, OnDestroy {
-    currentAccount: any;
     holdings: IHolding[];
-
-    holdingsCurrentUser: IHolding[];
-    error: any;
-    success: any;
+    currentAccount: any;
     eventSubscriber: Subscription;
     currentSearch: string;
-    routeData: any;
-    links: any;
-    totalItems: any;
-    queryCount: any;
-    itemsPerPage: any;
-    page: any;
-    predicate: any;
-    previousPage: any;
-    reverse: any;
 
     constructor(
         private holdingService: HoldingService,
-        private parseLinks: JhiParseLinks,
         private jhiAlertService: JhiAlertService,
-        private principal: Principal,
+        private dataUtils: JhiDataUtils,
+        private eventManager: JhiEventManager,
         private activatedRoute: ActivatedRoute,
-        private router: Router,
-        private eventManager: JhiEventManager
+        private principal: Principal
     ) {
-        this.itemsPerPage = ITEMS_PER_PAGE;
-        this.routeData = this.activatedRoute.data.subscribe(data => {
-            this.page = data.pagingParams.page;
-            this.previousPage = data.pagingParams.page;
-            this.reverse = data.pagingParams.ascending;
-            this.predicate = data.pagingParams.predicate;
-        });
-        this.holdingService.findByCurrentUser().subscribe(
-            (res: HttpResponse<IHolding[]>) => {
-                this.holdingsCurrentUser = res.body;
-            },
-            (res: HttpErrorResponse) => this.onError(res.message)
-        );
         this.currentSearch =
             this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search']
                 ? this.activatedRoute.snapshot.params['search']
@@ -65,75 +36,33 @@ export class HoldingComponent implements OnInit, OnDestroy {
         if (this.currentSearch) {
             this.holdingService
                 .search({
-                    page: this.page - 1,
-                    query: this.currentSearch,
-                    size: this.itemsPerPage,
-                    sort: this.sort()
+                    query: this.currentSearch
                 })
                 .subscribe(
-                    (res: HttpResponse<IHolding[]>) => this.paginateHoldings(res.body, res.headers),
+                    (res: HttpResponse<IHolding[]>) => (this.holdings = res.body),
                     (res: HttpErrorResponse) => this.onError(res.message)
                 );
             return;
         }
-        this.holdingService
-            .query({
-                page: this.page - 1,
-                size: this.itemsPerPage,
-                sort: this.sort()
-            })
-            .subscribe(
-                (res: HttpResponse<IHolding[]>) => this.paginateHoldings(res.body, res.headers),
-                (res: HttpErrorResponse) => this.onError(res.message)
-            );
-    }
-
-    loadPage(page: number) {
-        if (page !== this.previousPage) {
-            this.previousPage = page;
-            this.transition();
-        }
-    }
-
-    transition() {
-        this.router.navigate(['/holding'], {
-            queryParams: {
-                page: this.page,
-                size: this.itemsPerPage,
-                search: this.currentSearch,
-                sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
-            }
-        });
-        this.loadAll();
-    }
-
-    clear() {
-        this.page = 0;
-        this.currentSearch = '';
-        this.router.navigate([
-            '/holding',
-            {
-                page: this.page,
-                sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
-            }
-        ]);
-        this.loadAll();
+        this.holdingService.query().subscribe(
+            (res: HttpResponse<IHolding[]>) => {
+                this.holdings = res.body;
+                this.currentSearch = '';
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
     }
 
     search(query) {
         if (!query) {
             return this.clear();
         }
-        this.page = 0;
         this.currentSearch = query;
-        this.router.navigate([
-            '/holding',
-            {
-                search: this.currentSearch,
-                page: this.page,
-                sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
-            }
-        ]);
+        this.loadAll();
+    }
+
+    clear() {
+        this.currentSearch = '';
         this.loadAll();
     }
 
@@ -153,23 +82,16 @@ export class HoldingComponent implements OnInit, OnDestroy {
         return item.id;
     }
 
+    byteSize(field) {
+        return this.dataUtils.byteSize(field);
+    }
+
+    openFile(contentType, field) {
+        return this.dataUtils.openFile(contentType, field);
+    }
+
     registerChangeInHoldings() {
         this.eventSubscriber = this.eventManager.subscribe('holdingListModification', response => this.loadAll());
-    }
-
-    sort() {
-        const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
-        if (this.predicate !== 'id') {
-            result.push('id');
-        }
-        return result;
-    }
-
-    private paginateHoldings(data: IHolding[], headers: HttpHeaders) {
-        this.links = this.parseLinks.parse(headers.get('link'));
-        this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
-        this.queryCount = this.totalItems;
-        this.holdings = data;
     }
 
     private onError(errorMessage: string) {
